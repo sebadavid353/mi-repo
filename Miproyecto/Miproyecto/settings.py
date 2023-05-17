50d515d3f62f4db8aa113b985febfd5de27270f4
sebastian David <sebastiandn@hotmail.com>
2023-05-18 01:59:14 +0200

    prueba de vista y modelo en clientes

--- Miproyecto/Miproyecto/settings.py
+++ Miproyecto/Miproyecto/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cliente',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'Miproyecto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -62,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
             ],
         },
     },
@@ -103,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
--- Miproyecto/cliente/views.py
+++ Miproyecto/cliente/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Cliente
 
 # Create your views here.
+def index(request):
+    clientes = Cliente.objects.all()
+    datos = {'clientes':clientes}
+    return render(request, 'cliente/index_cliente.html',datos)
